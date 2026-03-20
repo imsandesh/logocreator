71e23b9b9732118767374bb0d0d170bd82b30cab
Sandesh eBrandz <sandesh@ebrandz.com>
2026-03-20 21:27:27 +0530

    disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,9 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  try {
-    const country = req.geo?.country || "UNKNOWN";
-
-    // Block Russia
-    if (country === "RU") {
-      return new NextResponse("Access Denied", { status: 403 });
-    }
-
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Middleware error:", error);
-
-    // Always fail-safe
-    return NextResponse.next();
-  }
+export function middleware() {
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  ],
+  matcher: ["/:path*"],
 };
