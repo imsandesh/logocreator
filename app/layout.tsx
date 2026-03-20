3de08fcbf3961593aa0c777c87589fd104af2f6b
Sandesh eBrandz <sandesh@ebrandz.com>
2026-03-20 21:40:58 +0530

    fix layout structure for nextjs app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,56 +10,28 @@ const jura = Jura({
   variable: "--font-jura",
 });
 
-const title = "Logo-creator.io – Generate a logo";
-const description = "Generate a logo for your company";
-const url = "https://www.logo-creator.io/";
-const ogimage = "https://www.logo-creator.io/og-image.png";
-const sitename = "logo-creator.io";
-
 export const metadata: Metadata = {
-  metadataBase: new URL(url),
-  title,
-  description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-  openGraph: {
-    images: [ogimage],
-    title,
-    description,
-    url: url,
-    siteName: sitename,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [ogimage],
-    title,
-    description,
-  },
+  title: "Logo-creator.io – Generate a logo",
+  description: "Generate a logo for your company",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
-      <html lang="en" className="h-full">
-        <head>
-          <PlausibleProvider domain="logo-creator.io" />
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <meta name="color-scheme" content="dark" />
-        </head>
-        <body
-          className={`${jura.variable} dark min-h-full bg-[#343434] font-jura antialiased`}
-        >
-          {children}
-          <Toaster />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className="h-full">
+      <body
+        className={`${jura.variable} dark min-h-full bg-[#343434] font-jura antialiased`}
+      >
+        <ClerkProvider>
+          <PlausibleProvider domain="logo-creator.io">
+            {children}
+            <Toaster />
+          </PlausibleProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
